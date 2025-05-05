fbdc198fa257051e66f929e86b14c6baa40b53f5
afree <afree@pc>
2025-05-05 15:52:24 +0530

    commited

--- samplereop/src/samplereop/Twoone.java
+++ samplereop/src/samplereop/Twoone.java
@@ -4,7 +4,7 @@ public class Twoone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Can't connect to any repository: https://github.com/shakeer2111/Samplerepo.git (https://github.com/shakeer2111/Samplerepo.git: not authorized)
 	}
 
 }
